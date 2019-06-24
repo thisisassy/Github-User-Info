03a40bcbc0ba211a7c57426f2964367fdbb0484d
ASSY <44109087+thisisassy@users.noreply.github.com>
2019-06-25 01:02:02 +0900

    init

--- index.js
+++ index.js
@@ -1,73 +1,72 @@
-function main() {
+function main(){
     const userId = getUserId();
     getUserInfo(userId)
-        .then((getUserInfo) => createView(getUserInfo))
-        .then((view) => displayView(view))
-        .catch((error) => {
-            console.error(`エラーが発生しました (${error})`);
-        });
+    //Promiseチェーン
+    .then((userInfo) => createView(userInfo))
+    .then((view) => displayView(view))
+    .catch((error) => {
+        console.log(`エラーが発生しました(${error})`);
+    });
 }
 
-function getUserInfo(userId) {
-    return new Promise((resolve, reject) => {
+function getUserInfo(userId){
+    return new Promise((resolve,reject) => {
         const request = new XMLHttpRequest();
-        request.open("GET", `https://api.github.com/users/${userId}`);
-        request.addEventListener("load", (event) => {
-            if (event.target.status !== 200) {
-                console.error(`${event.target.status}: ${event.target.statusText}`);
-                reject();
+        request.open("GET",`https://api.github.com/users/${userId}`);//''ではなく``
+        request.addEventListener("load",(event) => {
+            if(event.target.status !== 200){
+                reject(new Error(`${event.target.status}: ${event.target.statusText}`));
             }
             const userInfo = JSON.parse(event.target.responseText);
             resolve(userInfo);
         });
-        request.addEventListener("error", () => {
-            console.log("Network Error");
-            reject();
-        });
-        request.send();
+        request.addEventListener("error",() => {
+        reject(new Error("Network error"));
     });
-
+    request.send();
+});
 }
 
-function getUserId() {
+function getUserId(){
     const value = document.getElementById("userId").value;
     return encodeURIComponent(value);
 }
 
-function createView(userInfo) {
-    return escapeHTML ` 
-<img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-<h4>${userInfo.name} (@${userInfo.login})</h4>
-
-    <div>
-        <h4>Location</h4>
-        <p>${userInfo.location}</p>
-        <b>Repositories</b>
-        <p>${userInfo.public_repos}</p>
-    </div>`;
+function createView(userInfo){
+    return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+        <dt>Location</dt>
+        <dd>${userInfo.location}</dd>
+        <dt>Repositories</dt>
+        <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
 }
 
-function displayView(view) {
+function displayView(view){
     const result = document.getElementById("result");
     result.innerHTML = view;
 }
 
-function escapeSpecialChars(str) {
+//HTML文字列エスケープ
+function scapeSpecialChars(str){
     return str
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
+        .replace(/&/g,"&amp;")
+        .replace(/</g,"&lt;")
+        .replace(/>/g,"&gt;")
+        .replace(/"/g,"&quot;")
+        .replace(/'/g,"&#039;");
 }
 
-function escapeHTML(strings, ...values) {
-    return strings.reduce((result, string, i) => {
+function escapeHTML(strings,...values){
+    return strings.reduce((result,str,i)=> {
         const value = values[i - 1];
-        if (typeof value === "string") {
-            return result + escapeSpecialChars(value) + string;
-        } else {
-            return result + String(value) + string;
+        if(typeof value === "string"){
+            return result + scapeSpecialChars(value) + str;
+        }else{
+            return result + String(value) + str;
         }
     });
 }
